78d381c1e9c0a1e2aa2e5c519a705db11c3d38c7
w3r1 <46977742+w3r1@users.noreply.github.com>
2019-01-30 21:46:30 +0000

    Initial spring boot project checkin

---
+++ src/main/java/com/popularvenues/api/PopularVenuesApplication.java
@@ -0,0 +1,14 @@
+package com.popularvenues.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopularVenuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopularVenuesApplication.class, args);
+	}
+
+}
+
